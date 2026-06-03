bedfdbd63492f0ce13b5c58bd8a644bbf192ecf9
Vipul Gupta <vipulgupta92998@gmail.com>
2026-06-03 13:56:30 +0530

    url chnaged

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from "axios"
 
 
 export const api = axios.create({
-      baseURL: 'http://localhost:1111/', 
+      baseURL:[ 'http://localhost:1111/', 'https://explainoai-backend.onrender.com'],
     withCredentials:true
 })
