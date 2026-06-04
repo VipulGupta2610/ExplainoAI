f793e95994b92d7e3d7c993080d497a61cb63e81
Vipul Gupta <vipulgupta92998@gmail.com>
2026-06-04 14:42:58 +0530

    url update

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 
 app.use(cors({
 
-  origin: ["http://localhost:5173", "http://localhost:5174/"], 
+  origin: ["http://localhost:5173", "http://localhost:5174/","https://explainoai-frontend.onrender.com"], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true 
 }));
